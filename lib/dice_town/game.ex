e3d4f2962dce88bf6efd58bf6e7bab51f6b657e0
Mike <mdwaud@gmail.com>
2018-03-12 17:03:40 -0400

    add some payment stuff

--- lib/dice_town/game.ex
+++ lib/dice_town/game.ex
@@ -50,11 +50,48 @@ defmodule DiceTown.Game do
   def roll_dice(game_state) do
     result = Enum.random(1..6)
 
-    new_game_state = %GameState{game_state | turn: %GameTurn{
-      player_id: game_state.turn.player_id,
-      phase: :earn_income
-    }}
+    new_game_state = game_state
+    |> update_turn(game_state.turn.player_id, :earn_income)
 
     {:die_roll, result, new_game_state}
+  end
+
+  def earn_income(game_state, die_roll) do
+    new_game_state = game_state
+    |> wheat_field(die_roll)
+    |> update_turn(game_state.turn.player_id, :construction)
+
+    {:earned_income, [], new_game_state}
+  end
+
+  def wheat_field(game_state, 1) do
+    player_ids = game_state.players
+    |> Enum.map( fn(%Player{id: id}) -> id end)
+
+    pay_players(game_state, player_ids, 1)
+  end
+  def wheat_field(game_state, _), do: game_state
+
+  # utility methods
+  def pay_players(game_state, [], _), do: game_state
+  def pay_players(game_state, player_ids, amount) do
+    [player_id | other_player_ids] = player_ids
+
+    new_game_state = pay_player(game_state, player_id, amount)
+
+    pay_players(new_game_state, other_player_ids, amount)
+  end
+
+  def pay_player(game_state, player_id, amount) do
+    %GameState{game_state | coins: %{
+      game_state.coins | player_id => game_state.coins[player_id] + amount
+    }}
+  end
+
+  def update_turn(game_state, player_id, phase) do
+    %GameState{game_state | turn: %GameTurn{
+      player_id: player_id,
+      phase: phase
+    }}
   end
 end
