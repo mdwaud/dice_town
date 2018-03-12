709e805b13311b52c02fe46ca630000cfe0b630a
Mike <mdwaud@gmail.com>
2018-03-12 19:19:05 -0400

    pull income stuff into its own module

--- lib/dice_town/game.ex
+++ lib/dice_town/game.ex
@@ -1,4 +1,6 @@
 defmodule DiceTown.Game do
+  alias DiceTown.Game.EarnIncome
+
   # state struct
 
   defmodule Player do
@@ -61,77 +63,17 @@ defmodule DiceTown.Game do
   end
 
   def earn_income(game_state, die_roll) do
-    building_activations = calc_building_activiations(game_state.buildings_built, game_state.turn.player_id, die_roll)
+    building_activations = EarnIncome.calc_building_activiations(game_state.buildings_built, game_state.turn.player_id, die_roll)
 
     new_game_state = game_state
-    |> apply_building_activations(building_activations)
+    |> EarnIncome.apply_building_activations(building_activations)
     |> update_turn(game_state.turn.player_id, :construction)
 
     {:earned_income, [], new_game_state}
   end
 
-  def calc_building_activiations(buildings, current_player_id, die_roll) do
-    [:wheat_field, :bakery]
-    |> Enum.flat_map(fn(building) -> calc_building_activiations(buildings, current_player_id, die_roll, building) end)
-    |> List.flatten
-  end
-
-  def apply_building_activations(game_state, []), do: game_state
-
-  def apply_building_activations(game_state, building_activations) do
-    [building_activation | remaining_activations] = building_activations
-    new_game_state = pay_player(game_state, building_activation.to_player_id, building_activation.total_amount)
-
-    apply_building_activations(new_game_state, remaining_activations)
-  end
-
-  # buildings
-
-# figure out if the building is activated
-  def calc_building_activiations(buildings, _current_player_id, 1, :wheat_field) do
-    buildings
-    |> Enum.map(fn({player_id, building_map}) -> activate_building(building_map, player_id, :wheat_field) end)
-  end
-  def calc_building_activiations(_, _, _, :wheat_field), do: []
-
-  def calc_building_activiations(buildings, current_player_id, die_roll, :bakery) when die_roll >= 2 and die_roll <= 3 do
-    activate_building(buildings[current_player_id], current_player_id, :bakery)
-  end
-  def calc_building_activiations(_, _, _, :bakery), do: []
-
-# figure out the amount (if any)
-  def activate_building(%{wheat_field: count}, player_id, :wheat_field) when count > 0 do
-    [
-      %BuildingActivation{
-        building: :wheat_field,
-        count: count,
-        to_player_id: player_id,
-        from_player_id: nil,
-        total_amount: count
-      }
-    ]
-  end
-
-  def activate_building(%{bakery: count}, player_id, :bakery) when count > 0 do
-    [
-      %BuildingActivation{
-        building: :bakery,
-        count: count,
-        to_player_id: player_id,
-        from_player_id: nil,
-        total_amount: count
-      }
-    ]
-  end
-
 
   # utility methods
-
-  def pay_player(game_state, player_id, amount) do
-    %GameState{game_state | coins: %{
-      game_state.coins | player_id => game_state.coins[player_id] + amount
-    }}
-  end
 
   def update_turn(game_state, player_id, phase) do
     %GameState{game_state | turn: %GameTurn{
---
+++ test/dice_town/game/earn_income_test.exs
@@ -0,0 +1,84 @@
+defmodule DiceTown.Game.EarnIncomeTest do
+  use ExUnit.Case
+
+  alias DiceTown.Game
+  alias DiceTown.Game.EarnIncome
+
+  @initial_state_two_player %Game.GameState{
+    players: [
+      %Game.Player{
+        id: 0,
+        name: "Player 1"
+      },
+      %Game.Player{
+        id: 1,
+        name: "Player 2"
+      }
+    ],
+    buildings_built: %{
+      0 => %{
+        wheat_field: 1,
+        bakery: 1
+      },
+      1 => %{
+        wheat_field: 1,
+        bakery: 1
+      }
+    },
+    coins: %{
+      0 => 3,
+      1 => 3
+    },
+    buildings_available: %{
+      wheat_field: 8,
+      bakery: 8
+    },
+    turn: %Game.GameTurn{
+      player_id: 0,
+      phase: :roll_dice
+    }
+  }
+
+  describe "building activations" do
+    test "with two players and initial buildings should activate wheat" do
+      buildings = @initial_state_two_player.buildings_built
+      current_player_id = 0
+      die_roll = 1
+
+      building_activiations = EarnIncome.calc_building_activiations(buildings, current_player_id, die_roll)
+      assert building_activiations == [
+        %Game.BuildingActivation{
+          building: :wheat_field,
+          count: 1,
+          to_player_id: 0,
+          from_player_id: nil,
+          total_amount: 1
+        },
+        %Game.BuildingActivation{
+          building: :wheat_field,
+          count: 1,
+          to_player_id: 1,
+          from_player_id: nil,
+          total_amount: 1
+        }
+      ]
+    end
+
+    test "with two players and initial buildings should activate bakery" do
+      buildings = @initial_state_two_player.buildings_built
+      current_player_id = 0
+      die_roll = 2
+
+      building_activiations = EarnIncome.calc_building_activiations(buildings, current_player_id, die_roll)
+      assert building_activiations == [
+        %Game.BuildingActivation{
+          building: :bakery,
+          count: 1,
+          to_player_id: 0,
+          from_player_id: nil,
+          total_amount: 1
+        }
+      ]
+    end
+  end
+end
