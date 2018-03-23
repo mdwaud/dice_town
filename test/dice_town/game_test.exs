2bf94f16c7206d2ee2949e356c4395e67508a38a
Mike <mdwaud@gmail.com>
2018-03-23 09:56:37 -0400

    start to add test for cafe

--- test/dice_town/game_test.exs
+++ test/dice_town/game_test.exs
@@ -70,13 +70,37 @@ defmodule DiceTown.GameTest do
   test "rolling a 4 pays no one" do
   end
 
-  @tag :skip
-  test "rolling a 3 with no money (and someone owns a cafe) returns a failed EarnIncomeResult" do
+  describe "cafe" do
+    @tag :skip
+    test "rolling a 3 with money pays a cafe owner" do
+      # do setup
+      opts = %{player_names: ["Player 1", "Player 2"], die_fn: always_roll(3)}
+      game = start_supervised!({Game, opts})
+
+      {game_state, actions} = Game.roll_dice(game, 0)
+
+      # check game state
+      assert 0 == game_state[:players][0].coins
+      assert 1 == game_state[:players][1].coins
+      assert :construction == game_state[:phase]
+      assert 0 == game_state[:turn_player_id]
+      assert [0,1] == game_state[:player_order]
+      # check actions
+      assert actions == [
+        {:die_roll, 1},
+        {:earn_income, %{player_id: 1, from: {:player, 0}, building: :cafe, amount: 1}}
+      ]
+    end
+
+    @tag :skip
+    test "rolling a 3 with no money (and someone owns a cafe) returns a failed EarnIncomeResult" do
+    end
+
+    @tag :skip
+    test "handle partial cafe payment" do
+    end
   end
 
-  @tag :skip
-  test "handle partial cafe payment" do
-  end
 
   @tag :skip
   test "can buy a building" do
